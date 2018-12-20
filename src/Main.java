6c8b11e76472c5f94ddd0ccd9f2d73f38ef6f743
Robert Bennett <rbennett0721@gmail.com>
2018-12-20 14:20:43 -0500

    Setting up project.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * This is the main class for the game.
+ * I will add other classes for the player, enemies, etc., later.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
